63d8d2b245ae0b935745a21938955cfbbd2c3f50
fionn-mac <vedantsareen@gmail.com>
2018-06-29 19:12:41 +0530

    Make bookings.

--- skills/Helpers/Book.js
+++ skills/Helpers/Book.js
@@ -4,8 +4,64 @@
 
 const SALUTATIONS = [`Hey! Great to see you here.`, `Ola! Welcome back!`];
 const BOOKING_TIME = [`So, what time are we looking at?`];
+const BOOKING_TIME_AGAIN = [`Would you like to try for a different time?`];
 const BOOKING_DURATION = [`Cool! And for how long should I book the room?`];
+const DURATION_REMINDER = [`P.S.: Only 15 minutes or 30 minutes allowed! :wink:`]
+const UNAVAILABLE = [`Oops! The given time doesn't seem to be available :confused:`]
 const EXIT_MESSAGE = [`At any time, enter 'quit' to exit this conversation, yeah?`];
+const WRONG_TIME = [`I'm sorry, I couldn't understand the provided time. Are you sure it was in 24 hour HH:MM format?`];
+const WRONG_DURATION = [`The duration should either be 15 or 30 (minutes).`];
+const ASK_JOINEES = [`We're all set! Should I notify people on your behalf? (Unless you plan to ask them personally :wink:)`];
+
+let salutation = SALUTATIONS[Math.floor(Math.random()*SALUTATIONS.length)];
+let exitMessage = EXIT_MESSAGE[Math.floor(Math.random()*EXIT_MESSAGE.length)];
+let bookingTime = BOOKING_TIME[Math.floor(Math.random()*BOOKING_TIME.length)];
+let bookingTimeAgain = BOOKING_TIME_AGAIN[Math.floor(Math.random()*BOOKING_TIME_AGAIN.length)];
+let bookingDuration = BOOKING_DURATION[Math.floor(Math.random()*BOOKING_DURATION.length)];
+let durationReminder = DURATION_REMINDER[Math.floor(Math.random()*DURATION_REMINDER.length)];
+let unavailable = UNAVAILABLE[Math.floor(Math.random()*UNAVAILABLE.length)];
+let wrongTime = WRONG_TIME[Math.floor(Math.random()*WRONG_TIME.length)];
+let wrongDuration = WRONG_DURATION[Math.floor(Math.random()*WRONG_DURATION.length)];
+let askJoinees = ASK_JOINEES[Math.floor(Math.random()*ASK_JOINEES.length)];
+
+let time;
+let duration;
+let joinees;
+
+let timeCheck = new RegExp(`^(?:[0-9]|0[0-9]|1[0-9]|2[0-3]):[0-5][0-9]$`);
+let durationCheck = new RegExp(`15|30`);
+
+let workOnTime = function(response, convo) {
+  time = response.text;
+  if (timeCheck.test(time)) {
+    convo.gotoThread('askDuration');
+  }
+  convo.gotoThread('invalidTime');
+};
+
+let workOnDuration = function(response, convo) {
+  duration = response.text;
+  if (durationCheck.test(duration)) {
+    // TODO: Check availability.
+
+    if (available) {
+      // TODO : Make Booking!
+
+      convo.gotoThread('askJoinees');
+    } else {
+      convo.say(unavailable);
+      convo.gotoThread(`askTime`);
+    }
+  } else {
+    convo.gotoThread('invalidDuration');
+  }
+};
+
+let workOnJoinees =  function(response, convo) {
+  joinees = response.text;
+  convo.say(`You invited ${joinees}.`)
+  convo.next()
+}
 
 module.exports = function(bot, message) {
   let exitConvo = {
@@ -16,42 +72,47 @@ module.exports = function(bot, message) {
   };
 
   bot.createPrivateConversation(message, function(err, convo) {
-    let salutation = SALUTATIONS[Math.floor(Math.random()*SALUTATIONS.length)];
-    let exitMessage = EXIT_MESSAGE[Math.floor(Math.random()*EXIT_MESSAGE.length)];
-    let bookingTime = BOOKING_TIME[Math.floor(Math.random()*BOOKING_TIME.length)];
-    let bookingDuration = BOOKING_DURATION[Math.floor(Math.random()*BOOKING_DURATION.length)];
-
-    let time;
     convo.addQuestion(`${salutation}\n${exitMessage}\n${bookingTime}`, [
       exitConvo,
       {
         default: true,
-        callback: function(response, convo) {
-          time = response.text;
-          if (timeCheck.test(time)) {
-            convo.gotoThread('askDuration');
-          }
-          convo.gotoThread('invalidTime');
-        },
+        callback: workOnTime,
       },
     ], {}, 'default');
 
-    convo.addQuestion('', [
+    convo.addQuestion(wrongTime, [
       exitConvo,
       {
-        default: true,
-        callback: function(response, convo) {
-          targetChannel = response.text;
-          convo.gotoThread('getMessage');
-        },
+        callback: workOnTime,
+    ], {}, 'invalidTime');
+
+    convo.addQuestion(bookingTimeAgain, [
+      exitConvo,
+      {
+        callback: workOnTime,
+    ], {}, 'askTime');
+
+    convo.addQuestion(`${bookingDuration}\n${durationReminder}`, [
+      exitConvo,
+      {
+        callback: workOnDuration,
       },
     ], {}, 'askDuration');
 
-    convo.activate();
+    convo.addQuestion(wrongDuration, [
+      exitConvo,
+      {
+        callback: workOnDuration,
+      },
+    ], {}, 'invalidDuration');
 
-    responses = convo.extractResponses();
-    console.log("RESPONSESSSS");
-    console.log(responses);
-  });
+    convo.addQuestion(askJoinees, [
+      exitConvo,
+      {
+        callback: workOnJoinees,
+      },
+    ], {}, 'askJoinees');
+
+    convo.activate();
   });
 }
